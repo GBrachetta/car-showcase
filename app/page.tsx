ea325511613ff248114d3bb2c3d32d36ac031fb1
Guillermo Brachetta <brachetta@me.com>
2023-06-10 21:45:27 +0200

    Add CarCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import CarCard from '@/components/CarCard';
 import CustomFilter from '@/components/CustomFilter';
 import Hero from '@/components/Hero';
 import SearchBar from '@/components/SearchBar';
+import { fetchCars } from '@/utils';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -22,6 +28,23 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, i) => (
+                <CarCard car={car} key={i} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-xl font-bold text-black">
+              There are no results
+            </h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
