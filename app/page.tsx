47cfd33398544a988e5bde6a22861d097c0ffdc2
Guillermo Brachetta <brachetta@me.com>
2023-09-08 13:55:25 +0200

    Add handleScroll function

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default async function Home({ searchParams }: HomeProps) {
 
   return (
     <main className="overflow-hidden">
+      {' '}
       <Hero />
-
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
